3113d3f623ffb8610e301912d8073e995158d899
4v0v <30574115+4v0v@users.noreply.github.com>
2020-05-19 02:22:39 +0200

    better trail

--- entities/trail.lua
+++ entities/trail.lua
@@ -1,48 +1,40 @@
 Trail = Entity:extend('Trail')
 
-function Trail:new(x, y)
+function Trail:new(x, y, target_x, target_y)
     Trail:super().new(self, x, y, 1)
 
-    self.t              = vec2()
-    self.pos            = vec2(x, y)
-    self.vel            = vec2(tools.random(-1, 1), tools.random(-1, 1))
-    self.target         = vec2(dx, dy)
-    self.turn_speed     = 100
-    self.move_speed     = tools.random(1, 100)
-    self.visible        = 10
-    
+    self.target      = vec2(target_x, target_y)
+    self.position    = vec2(x, y)
+    self.direction   = vec2(tools.random(-1, 1), tools.random(-1, 1))
+    self.speed       = tools.random(1, 100)
+    self.turn_speed  = 0
+
     self.trail      = {}
     for i = 1, 6 do 
-        table.insert( self.trail, self.pos.x)
-        table.insert( self.trail, self.pos.y)
+        table.insert( self.trail, self.position.x)
+        table.insert( self.trail, self.position.y)
     end
 end
 
 function Trail:init() 
-    self.timer:tween(0.5, self, {move_speed = 1500}, 'linear')
-    self.timer:tween(0.5, self, {turn_speed = 5}, 'out-quad')
+    self.timer:tween(0.5, self, {speed = 1500}, 'linear')
+    self.timer:tween(1, self, {turn_speed = 5}, 'out-quad')
 end
 
 function Trail:update(dt)
     Trail:super().update(self, dt)
 
-    self.target = (self.t - self.pos):normalize()
-    local steering = self.target - self.vel
-    self.vel = self.vel + (steering / self.turn_speed)
-    self.pos = self.pos + self.vel * self.move_speed * dt
+    local target_direction = (self.target - self.position):normalize()
+    local direction_difference = target_direction - self.direction
+    self.direction += (direction_difference * (self.turn_speed / 100))
+    self.position += self.direction * self.speed * dt
 
-    -- visual
     table.remove( self.trail )
     table.remove( self.trail )
-    table.insert( self.trail,1, math.ceil(self.pos['y']))
-    table.insert( self.trail,1, math.ceil(self.pos['x']))
+    table.insert( self.trail,1, math.ceil(self.position['y']))
+    table.insert( self.trail,1, math.ceil(self.position['x']))
 
-    -- prevent visual bug on creation
-    if type(self.visible) == 'number' then self.visible = self.visible - 1 end
-    if self.visible == 0 then self.visible = true end
-
-    -- non instant deletion
-    if tools.almost(self.pos.x, self.t.x, 15) and tools.almost(self.pos.y, self.t.y, 15) then 
+    if tools.almost(self.position.x, self.target.x, 20) and tools.almost(self.position.y, self.target.y, 20) then 
         table.remove( self.trail )
         table.remove( self.trail )
         if #self.trail == 0 then 
@@ -53,43 +45,44 @@ function Trail:update(dt)
 end
 
 function Trail:draw()
-
+    lg.setLineWidth(3)
     if #self.trail > 4 then 
-        lg.setColor(1,0,0,0.8)
+        lg.setColor(1,0,0,1)
         lg.line(self.trail[1] +2, self.trail[2] +2, self.trail[3] +2 , self.trail[4]+2)
         lg.setColor(1,1,1,1)
         lg.line(self.trail[1], self.trail[2], self.trail[3], self.trail[4])
     end
     
     if #self.trail > 6 then 
-        lg.setColor(1,0,0,0.8)
+        lg.setColor(1,0,0,1)
         lg.line(self.trail[3] +2, self.trail[4] +2, self.trail[5] +2 , self.trail[6]+2)
         lg.setColor(1,1,1,1)
         lg.line(self.trail[3], self.trail[4], self.trail[5], self.trail[6])
     end
 
     if #self.trail > 8 then 
-        lg.setColor(1,0,0, 0.4)
+        lg.setColor(1,0,0, 0.6)
         lg.line(self.trail[5] +2, self.trail[6] +2, self.trail[7] +2 , self.trail[8]+2)
         lg.setColor(1,1,1,0.6)
         lg.line(self.trail[5], self.trail[6], self.trail[7], self.trail[8])
     end
 
     if #self.trail > 10 then 
-        lg.setColor(1,0,0, 0.4)
+        lg.setColor(1,0,0, 0.6)
         lg.line(self.trail[7] +2, self.trail[8] +2, self.trail[9] +2, self.trail[10]+2)
         lg.setColor(1,1,1,0.6)
         lg.line(self.trail[7], self.trail[8], self.trail[9], self.trail[10])
     end
 
     if #self.trail > 12 then 
-        lg.setColor(1,0,0, 0.1)
+        lg.setColor(1,0,0, 0.2)
         lg.line(self.trail[9] +2, self.trail[10] +2, self.trail[11] +2 , self.trail[12]+2)
         lg.setColor(1,1,1,0.2)
         lg.line(self.trail[9], self.trail[10], self.trail[11] , self.trail[12])
     end
+    lg.setLineWidth(1)
 
     lg.setColor(1,1,1,1)
 end
 
-function Trail:set_target(x, y) self.t.x, self.t.y = x, y  end
+function Trail:set_target(x, y) self.target.x, self.target.y = x, y  end
